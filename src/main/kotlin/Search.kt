c04405aa9c8ee9333b2e313ca877aa2e271d9dfe
Ibrahim Alshubaily <ibrahim.alshubaily.sde@gmail.com>
2022-12-01 18:26:50 +0000

    binarySearch

--- src/main/kotlin/Search.kt
+++ src/main/kotlin/Search.kt
@@ -2,8 +2,8 @@ import kotlin.math.sqrt
 
 class Search {
 
-    companion object{
-        fun twoCrystalBalls(input: Array<Boolean>): Int{
+    companion object {
+        fun twoCrystalBalls(input: Array<Boolean>): Int {
             val stepSize = sqrt(input.size.toDouble()).toInt()
             var curIdx = stepSize
             while (!input[curIdx])
@@ -12,6 +12,22 @@ class Search {
             while (!input[curIdx])
                 curIdx++
             return curIdx
+        }
+
+        fun binarySearch(input: Array<Int>, target: Int): Int {
+            var lo = 0
+            var hi = input.size
+            while (lo < hi) {
+                val mid = lo + (hi - lo) / 2
+                if (input[mid] == target) {
+                    return mid
+                } else if (input[mid] < target) {
+                    lo = mid + 1
+                } else {
+                    hi = mid
+                }
+            }
+            return -1
         }
     }
 }
--- src/test/kotlin/SearchTest.kt
+++ src/test/kotlin/SearchTest.kt
@@ -5,7 +5,16 @@ import org.junit.jupiter.api.Assertions.*
 internal class SearchTest {
     @Test
     fun twoGlassSearchTest() {
-        val input = Array<Boolean>(100) { idx -> idx >= 69 }
+        val input = Array(100) { idx -> idx >= 69 }
         assertEquals(Search.twoCrystalBalls(input), 69)
+    }
+
+    @Test
+    fun binarySearchTest() {
+        val input = Array(100) { idx -> idx }
+        assertEquals(Search.binarySearch(input, -1), -1)
+        for (v in input)
+            assertEquals(Search.binarySearch(input, v), v)
+        assertEquals(Search.binarySearch(input, 100), -1)
     }
 }
